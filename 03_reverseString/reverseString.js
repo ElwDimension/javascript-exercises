b4df9a94aaad46051cc9616afcceaca5ea52ef71
ElwDimension <ElwDimension@gmail.com>
2023-03-23 15:46:08 -0400

    Completed exercises 1-3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
-};
+function reverseString(phrase){
+    let array=phrase.split("");
+    let result="";
+    for(let i=array.length-1;i>=0;i--){
+        result+=array[i];
+    }
+    return result;
+}
 
 // Do not edit below this line
 module.exports = reverseString;

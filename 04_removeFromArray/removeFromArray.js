848ffe267f119ee23fc4428bf6eecc3dd5332b6d
ElwDimension <ElwDimension@gmail.com>
2023-03-27 13:49:29 -0400

    completed removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
-const removeFromArray = function() {
+/*function removeFromArray(array,target){
+    let result = [];
+    for (let i=0;i<array.length;i++){
+        
+        if(array[i]!=target){
+            result.push(array[i]);
+        }       
+    }
+    return result;
+}
+*/
 
-};
+function removeFromArray(array,...args){
+        let result=[];
+        for (let i=0;i<array.length;i++){
+            if(!args.includes(array[i])){
+                result.push(array[i]);
+            }
+        }
+        return result;
+    }
+
 
 // Do not edit below this line
 module.exports = removeFromArray;

2b12c28e8d5e4b42bf170c1d70f0a7735f60bae6
ElwDimension <ElwDimension@gmail.com>
2023-03-27 15:39:17 -0400

    Completed exercise 09 - Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    const shortPhrase = phrase
+        .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+        .replace(/\s+/g, '')
+        .toUpperCase();
+    return shortPhrase===reverseString(shortPhrase);  
 };
+
+function reverseString(phrase){
+    let array=phrase.split("");
+    let result="";
+    for(let i=array.length-1;i>=0;i--){
+        result+=array[i];
+    }
+    return result;
+}
 
 // Do not edit below this line
 module.exports = palindromes;

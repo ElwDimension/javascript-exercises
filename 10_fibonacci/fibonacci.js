16f4c9b4bcfc1e796655eaa8c69c463a525f80ad
ElwDimension <ElwDimension@gmail.com>
2023-03-28 14:03:11 -0400

    Completed exercise 10 -fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    x=Number(x);
+    if(x<0){return "OOPS";}
+
+    let sum=1;
+    let first=0;
+    let second=1;
+    for (let i=1;i<x;i++){
+        sum=first+second;
+        first=second;
+        second=sum;
+    }
+    return sum;
+    
 
 };
 

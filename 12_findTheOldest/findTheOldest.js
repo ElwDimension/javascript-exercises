57a9b5badb31816cfe3a85c751d65241b9607399
ElwDimension <ElwDimension@gmail.com>
2023-04-26 14:55:30 -0400

    Completed remaining exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const currentYear=new Date().getFullYear();
 
+const findTheOldest = function(people) {
+
+
+    return people.reduce((oldest, next) => {
+        let oldestAge=getAge(oldest.yearOfBirth,oldest.yearOfDeath);
+        let nextAge=getAge(next.yearOfBirth,next.yearOfDeath);
+        return oldestAge > nextAge ? oldest : next;
+    });
+
+};
+
+const getAge = function(birth, death){
+    if(!death){
+        return currentYear-birth;    
+        
+    }
+    else{
+        return death-birth;
+    }
 };
 
 // Do not edit below this line
